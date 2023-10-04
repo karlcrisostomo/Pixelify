0b827a2ddcb2e16d37dcf6b90879a720dd6c79cd
KarlCrisostomo12 <101928489+KarlCrisostomo12@users.noreply.github.com>
2023-10-04 19:36:34 +0800

    ...

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains: ["images.pexels.com"]
-    }
-
-
+  images: {
+    domains: ["images.pexels.com"],
+  },
+  reactStrictMode: false,
 };
 
 module.exports = nextConfig;
--- pages/home.js
+++ pages/home.js
@@ -1,49 +1,58 @@
 import { useFeaturedImages } from "./api/featured";
 import { useSearchContext } from "@/context/SearchContext";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Photos } from "@/components";
 
 const Home = () => {
-  const { searchResult, handleLoadMore } = useSearchContext();
+  const { searchResult, handleLoadMore } = useSearchContext() || {};
   const [isHovered, setHovered] = useState(null);
+  const [searchResultsExist, setSearchResultsExist] = useState(false);
 
   const featuredImages = useFeaturedImages(); // Get the random images
+
+  // Check if searchResult exists when it changes
+
+  useEffect(() => {
+    if (searchResult && searchResult.length > 0) {
+      setSearchResultsExist(true);
+    } else {
+      setSearchResultsExist(false);
+    }
+  }, [searchResult]);
 
   return (
     <>
       <section className="py-16">
         <div className="styled_container">
-          {searchResult?.length && searchResult.length > 0 ? (
+          {searchResultsExist || featuredImages.length > 0 ? (
             <div className="image_container">
-              {searchResult.map((image, index) => (
-                <Photos
-                  src={image.src.large}
-                  alt={image.photographer}
-                  isHovered={isHovered}
-                  setHovered={setHovered}
-                  index={index}
-                  key={index}
-                />
-              ))}
-            </div>
-          ) : featuredImages.length > 0 ? (
-            <div className="image_container">
-              {featuredImages.map((image, index) => (
-                <Photos
-                  src={image.src.large}
-                  alt={image.photographer}
-                  isHovered={isHovered}
-                  setHovered={setHovered}
-                  index={index}
-                  key={index}
-                />
-              ))}
+              {searchResultsExist
+                ? searchResult.map((image, index) => (
+                    <Photos
+                      src={image.src.large}
+                      alt={image.photographer}
+                      isHovered={isHovered}
+                      setHovered={setHovered}
+                      index={index}
+                      key={index}
+                    />
+                  ))
+                : featuredImages.map((image, index) => (
+                    <Photos
+                      src={image.src.large}
+                      alt={image.photographer}
+                      isHovered={isHovered}
+                      setHovered={setHovered}
+                      index={index}
+                      key={index}
+                    />
+                  ))}
             </div>
           ) : (
             <div>No search results available</div>
           )}
 
-          {searchResult?.length > 0 && (
+          {searchResultsExist && (
             <div className="flex justify-center mt-12">
               <button
                 className="font-medium dark:bg-gray-900 hover:-translate-y-1 hover:-translate-x-1 hover:shadow-xl hover:shadow-gray-600 tracking-wider transition-all hover:scale-105 hover:font-bold text-white p-4 rounded-lg"
