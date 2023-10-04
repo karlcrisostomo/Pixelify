1e0ea0dbeff56339defa75635487250ddeaa18d3
KarlCrisostomo12 <101928489+KarlCrisostomo12@users.noreply.github.com>
2023-10-04 18:11:02 +0800

    ...

--- pages/home.js
+++ pages/home.js
@@ -13,22 +13,25 @@ const Home = () => {
     <>
       <section className="py-16">
         <div className="styled_container">
-          {(searchResult.length > 0 || featuredImages.length > 0) && (
+          {((searchResult && searchResult.length > 0) ||
+            featuredImages.length > 0) && (
             <div className="image_container">
-              {(searchResult.length > 0 ? searchResult : featuredImages).map((image, index) => (
-                <Photos
-                  src={image.src.large}
-                  alt={image.photographer}
-                  isHovered={isHovered}
-                  setHovered={setHovered}
-                  index={index}
-                  key={index}
-                />
-              ))}
+              {(searchResult.length > 0 ? searchResult : featuredImages).map(
+                (image, index) => (
+                  <Photos
+                    src={image.src.large}
+                    alt={image.photographer}
+                    isHovered={isHovered}
+                    setHovered={setHovered}
+                    index={index}
+                    key={index}
+                  />
+                )
+              )}
             </div>
           )}
 
-          {searchResult.length > 0 && (
+          {searchResult && searchResult.length > 0 && (
             <div className="flex justify-center mt-12">
               <button
                 className="font-medium dark:bg-gray-900 hover:-translate-y-1 hover:-translate-x-1 hover:shadow-xl hover:shadow-gray-600 tracking-wider transition-all hover:scale-105 hover:font-bold text-white p-4 rounded-lg"
